3e49f604c6321f2967a34770ea1b0601bd38abe5
Diego F <jethrotul@gmail.com>
2022-10-25 17:37:04 +0200

    cosas

---
+++ M03/Practica Crucigrama/PracticaCrucigrama/src/main/java/cat/copernic/practicacrucigrama/SystemInfo.java
@@ -0,0 +1,13 @@
+package cat.copernic.practicacrucigrama;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ M03/Practica Crucigrama/PracticaCrucigrama/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module cat.copernic.practicacrucigrama {
+    requires javafx.controls;
+    exports cat.copernic.practicacrucigrama;
+}
